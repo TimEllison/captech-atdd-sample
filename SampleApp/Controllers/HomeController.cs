4acc9bd726a7418bb813ecf7fbd2c46800c0df7b
TimEllison <tellison@captechconsulting.com>
2015-10-27 09:55:36 -0400

    form done.

--- SampleApp/Controllers/HomeController.cs
+++ SampleApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SampleApp.Models;
 
 namespace SampleApp.Controllers
 {
@@ -22,9 +23,49 @@ namespace SampleApp.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Submit to Contact Us";
 
+            return View(new ContactModel());
+        }
+
+        public ActionResult Save()
+        {
+            return RedirectToAction("Saved");
+        }
+
+        public ActionResult Saved()
+        {
             return View();
+        }
+
+        public ActionResult Counties(string id)
+        {
+            var counties = new List<KeyValuePair<string,string>>();
+            switch (id)
+            {
+                case "VA":
+                    counties.AddRange(new KeyValuePair<string,string>[]
+                    {
+                        new KeyValuePair<string, string>("H", "Henrico"),
+                        new KeyValuePair<string, string>("HA", "Hanover"),
+                        new KeyValuePair<string, string>("NK", "New Kent"),
+                        new KeyValuePair<string, string>("G", "Goochland"),
+                    });
+                    break;
+                case "NC":
+                    counties.AddRange(new KeyValuePair<string, string>[]
+                    {
+                        new KeyValuePair<string, string>("HAL", "Halifax"),
+                        new KeyValuePair<string, string>("W", "Wake"),
+                        new KeyValuePair<string, string>("WA", "Wayne"),
+                        new KeyValuePair<string, string>("C", "Catawba"),
+                    });
+                    break;
+                default:
+                    counties = null;
+                    break;
+            }
+            return Json(counties, JsonRequestBehavior.AllowGet);
         }
     }
 }
